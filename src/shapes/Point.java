c88b43f0a4b8865780e49539f0e1a4ca117b9461
Aiana <aiana.baitakova.j35@fh-zwickau.de>
2019-03-21 19:32:35 +0100

    Added class Point

---
+++ src/shapes/Point.java
@@ -0,0 +1,32 @@
+package shapes;
+
+public class Point {
+
+	private int x;
+	private int y;
+	
+	Point(int x, int y){
+		
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public Point move(int x, int y){
+		return null;
+	}
+	
+}
