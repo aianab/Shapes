7147732f366d09eccf06b0f67e0b213abc63fcdf
Aiana <aiana.baitakova.j35@fh-zwickau.de>
2019-03-21 19:03:17 +0100

    Updated Drawable and Figure

--- src/shapes/Drawable.java
+++ src/shapes/Drawable.java
@@ -1,5 +1,6 @@
 package shapes;
 
 public interface Drawable {
-
+void draw();
+Drawable move(int x, int y);
 }
--- src/shapes/Figure.java
+++ src/shapes/Figure.java
@@ -1,5 +1,29 @@
 package shapes;
 
-public class Figure {
+public class Figure implements Drawable{
+	
+	Figure(){
+		
+	}
+	Figure addShape(Drawable o) {
+		return null;
+		
+	}
+	
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Drawable move(int x, int y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private Figure snowMan() {
+		return null;
+	}
 
 }
