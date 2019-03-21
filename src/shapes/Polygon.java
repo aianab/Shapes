409dd59b290d1f4f2405c5270d247c6134e605a9
mur18j38 <mur18j38>
2019-03-21 19:36:10 +0100

    added class Polygon

---
+++ src/shapes/Polygon.java
@@ -0,0 +1,30 @@
+package shapes;
+
+public class Polygon extends Shape {
+	private ArrayList<Point> points = new ArrayList<>();
+	
+	public Polygon() {
+		
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Drawable move(int x, int y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public void setPoints(ArrayList<Point> points) {
+		this.points = points;
+	}
+	
+	public ArrayList<Point> getPoints() {
+		return this.points;
+	}
+	
+}
