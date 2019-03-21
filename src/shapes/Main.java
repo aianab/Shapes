2cb8312a7a68225019ad260783cd207c1ca32baa
Aiana <aiana.baitakova.j35@fh-zwickau.de>
2019-03-21 20:23:09 +0100

    Added class Rectangle

--- src/shapes/Main.java
+++ src/shapes/Main.java
@@ -1,7 +1,11 @@
 package shapes;
-
+import teaching.*;
 public class Main {
 	public static void main(String ...args) {
 		
+		Point centerPoint = new Point(50,50);
+		Rectangle rect = new Rectangle(centerPoint, 100, 200);
+		WhiteBoard wb = new WhiteBoard();
+		wb.drawRectangle(50, 50, 100, 150);
 	}
 }
