f88071c37f30308c956e0a0f9b3ee98f6a571a67
mur18j38 <mur18j38>
2019-03-21 20:54:20 +0100

    Class Line implemented

--- src/shapes/Line.java
+++ src/shapes/Line.java
@@ -8,4 +8,15 @@ public class Line extends Polygon {
 		this.p1 = p1;
 		this.p2 = p2;
 	}
+	
+	public void draw() {
+		getWhiteBoard().drawLine(p1.getX(), p1.getY(), p2.getX(), p2.getY());
+	}
+	
+	public Drawable move(int x, int y) {
+		getWhiteBoard().removeShape(this);
+		p1 = new Point(p1.getX() + x, p1.getY() + y);
+		p2 = new Point(p2.getX() + x, p2.getY() + y);
+		return this;
+	}
 }
