9b1f61434bb21ebca9c879a56b47d32091187f8d
Aiana <aiana.baitakova.j35@fh-zwickau.de>
2019-03-24 14:55:08 +0100

    Updated class Rectangle and Figure

--- src/shapes/Figure.java
+++ src/shapes/Figure.java
@@ -1,29 +1,51 @@
 package shapes;
+import java.util.HashSet;
+import java.util.Set;
 
+import teaching.*;
 public class Figure implements Drawable{
 	
+	private Set<Drawable> shapes = new HashSet<Drawable>();
+	
+	private Polygon polygon;
+	
 	Figure(){
-		
+		polygon = new Polygon();
 	}
+	
 	Figure addShape(Drawable o) {
-		return null;
+		shapes.add(o);
 		
-	}
+		return this;
+		}
 	
 	@Override
 	public void draw() {
-		// TODO Auto-generated method stub
-		
+		this.removeShapes();
+		for (Drawable drawable : shapes) {
+			drawable.draw();
+		}
 	}
+	
 
 	@Override
 	public Drawable move(int x, int y) {
-		// TODO Auto-generated method stub
-		return null;
+		for (Drawable drawable : shapes) {
+			drawable.move(x, y);
+		}
+		return this;
 	}
 	
 	public Figure snowMan() {
+		//Circle bigCircle = new Circle();
+		
 		return null;
+	}
+	
+	private void removeShapes() {
+		for (Drawable drawable : shapes) {
+			polygon.getWhiteBoard().removeShape(drawable);
+		}
 	}
 
 }
